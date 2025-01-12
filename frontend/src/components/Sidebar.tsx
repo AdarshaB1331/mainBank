359a7caad8ea7d8aae5401a3eac024d388b9eab7
adarsha <022BSCIT005@sxc.edu.np>
2025-01-12 15:21:59 +0545

    made changes to the sidebar.tsx users list name to users list

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -63,7 +63,7 @@ const Sidebar = () => {
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#table" />
             </svg>
-            Users List
+            User's List
           </NavLink>
         </li>
         <li>
